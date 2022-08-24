4c9c938f62dfd5ecd3c01becd1292086a0255492
mfjkri <muhammadfikri6844@gmail.com>
2022-08-24 09:02:15 +0800

    Added alternative CanSum using tabulation method

--- 6-DynamicProgramming/Materials/code/03_CanSum/CanSumTab.py
+++ 6-DynamicProgramming/Materials/code/03_CanSum/CanSumTab.py
@@ -7,9 +7,28 @@ def canSumTab(array, target):
             for number in array:
                 if (i + number) < len(table):
                     table[i + number] = True
-    
+
     return table[target]
+
+
+def canSumTabAlt(arr, target):
+    table = [True] * (target + 1)
+
+    for i in range(1, target + 1):
+        ans = False
+
+        for toSubstrat in arr:
+
+            if i - toSubstrat < 0:
+                continue
+            if table[i - toSubstrat]:
+                print(table)
+                print(i, toSubstrat)
+                ans = True
+                break
+        table[i] = ans
+    return table[target]
+
 
 print(canSumTab([3, 6, 7], 1000))
 print(canSumTab([7, 14], 300))
-        
